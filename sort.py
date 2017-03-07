d03695513658885121859edc6aa884079be90dde
wangqihui <wangqihui0324@gmail.com>
2017-03-07 16:07:29 +0800

    add other sort

--- sort.py
+++ sort.py
@@ -14,23 +14,45 @@ def quicksort(a):
 
 # 插入排序
 def insert_sort(lst):
-    n=len(lst)
-    if n==1:
+    n = len(lst)
+    if n <= 1:
         return lst
-    for i in range(1,n):
-        for j in range(i,0,-1):
-            if lst[j]<lst[j-1]: lst[j],lst[j-1]=lst[j-1],lst[j]
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if lst[j] < lst[j-1]:
+                lst[j], lst[j-1]=lst[j-1], lst[j]
     return lst
 
 
+# 冒泡排序
+def bubble_sort(lst):
+    for j in range(len(lst)-1,0,-1):
+        for i in range(0, j):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+    return lst
 
+
+# 选择排序
+def selection_sort(lst):
+    n = len(lst)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if lst[min_index] > lst[j]:
+                min_index = j
+        if min_index != i:
+            lst[min_index], lst[i] = lst[i], lst[min_index]
+    return lst
 
 
 
 def main():
     lst = [12,4,5,6,7,3,6,1,15]
     # print quicksort(lst)
-    print insert_sort(lst)
+    # print insert_sort(lst)
+    # print bubble_sort(lst)
+    print selection_sort(lst)
 
 
 if __name__ == '__main__':
