704035903a578a0e964556ad4af4fefd8a396f33
wangqihui <wangqihui0324@gmail.com>
2017-03-07 14:43:54 +0800

    fix a bug

--- sort.py
+++ sort.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+
+# quicksort
+
 # Wikipedia
+# 想去掉重复值只需把'='去掉即可
 def quicksort(a):
     if len(a) <= 1:
         return a
-    return quicksort([x for x in a[1:] if x < a[0]]) + [a[0]] + quicksort([x for x in a[1:] if x > a[0]])
+    return quicksort([x for x in a[1:] if x <= a[0]]) + [a[0]] + quicksort([x for x in a[1:] if x > a[0]])
+
+
+
+
+
 
 
 
 def main():
-    print quicksort([12,4,5,6,7,3,1,15])
+    print quicksort([12,4,5,6,7,3,6,1,15])
+
 
 if __name__ == '__main__':
     main()
