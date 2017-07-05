00e1d45ef51d122d1e8c7e033ef5660c6c287db8
wangqihui <wangqihui0324@gmail.com>
2017-07-05 17:04:01 +0800

    kmp edit

--- kmp.py
+++ kmp.py
@@ -1,41 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# 朴素匹配
-
+# 简单匹配
 def naive_match(s, p):
     m = len(s)
     n = len(p)
-    for i in range(m-n+1):#起始指针i
-        if s[i:i+n] == p:
+    for i in range(m-n+1):
+        if s[i: i+n] == p:
             return True
     return False
 
-# KMP
+# 部分匹配表
+def partial_table(p):
+    prefix = set()
+    ret = [0]
+    for i in range(1, len(p)):
+        prefix.add(p[: i])
+        postfix = {p[j: i + 1] for j in range(1, i + 1)}
+        s_list = [len(s) for s in (prefix & postfix)]
+        ret.append(max(s_list) if s_list else 0)
+    return ret
 
+# KMP
 def kmp_match(s, p):
     m = len(s)
     n = len(p)
-    cur = 0  # 起始指针cur
+    cur = 0
     table = partial_table(p)
     while cur <= m - n:
         for i in range(n):
             if s[i + cur] != p[i]:
-                cur += max(i - table[i - 1], 1)  # 有了部分匹配表,我们不只是单纯的1位1位往右移,可以一次移动多位
+                cur += max(i - table[i - 1], 1)
                 break
         else:
             return True
     return False
-
-
-# 部分匹配表
-
-def partial_table(p):
-    prefix = set()
-    postfix = set()
-    ret = [0]
-    for i in range(1, len(p)):
-        prefix.add(p[:i])
-        postfix = {p[j:i + 1] for j in range(1, i + 1)}
-        ret.append(len((prefix & postfix or {''}).pop()))
-    return ret
