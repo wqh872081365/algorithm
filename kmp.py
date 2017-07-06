d11a4f0378af97bc503762673528260057faf16a
wangqihui <wangqihui0324@gmail.com>
2017-07-06 13:10:54 +0800

    add kmp table

--- kmp.py
+++ kmp.py
@@ -10,8 +10,8 @@ def naive_match(s, p):
             return True
     return False
 
-# 部分匹配表
-def partial_table(p):
+# 简单部分匹配表
+def partial_table_simple(p):
     prefix = set()
     ret = [0]
     for i in range(1, len(p)):
@@ -19,6 +19,18 @@ def partial_table(p):
         postfix = {p[j: i + 1] for j in range(1, i + 1)}
         s_list = [len(s) for s in (prefix & postfix)]
         ret.append(max(s_list) if s_list else 0)
+    return ret
+
+# 部分匹配表
+def partial_table(p):
+    ret = [0]
+    for i in range(1, len(p)):
+        t = ret[i-1]
+        while t > 0 and p[t] != p[i]:
+            t = ret[t-1]
+        if p[i] == p[t]:
+            t += 1
+        ret.append(t)
     return ret
 
 # KMP
@@ -35,3 +47,5 @@ def kmp_match(s, p):
         else:
             return True
     return False
+
+print(partial_table("aacecaaa#aaacecaa"))
